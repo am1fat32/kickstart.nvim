f7354f24c85379ceafe59a2a3f233acdddfcc7c5
fat32 <fat32dev@gmail.com>
2024-05-02 23:34:51 +0400

    Add mini-move plugin

---
+++ lua/custom/plugins/mini-move.lua
@@ -0,0 +1,9 @@
+return {
+  'echasnovski/mini.move',
+  version = '*',
+  config = function()
+    require('mini.move').setup {
+      -- Configuration here, or leave empty to use defaults
+    }
+  end,
+}
