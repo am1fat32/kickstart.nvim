4e2275cf20e0619cf8b6db740adc14b53f9ccf20
fat32 <fat32dev@gmail.com>
2024-05-05 00:45:58 +0400

    Remove noice plugin

---
+++ lua/custom/plugins/noice.lua
@@ -0,0 +1,30 @@
+return {
+  -- 'folke/noice.nvim',
+  -- event = 'VeryLazy',
+  -- opts = {
+  --   messages = {
+  --     enabled = false,
+  --   },
+  -- },
+  -- dependencies = {
+  --   'MunifTanjim/nui.nvim',
+  -- },
+  -- config = function()
+  --   require('noice').setup {
+  --     hover = {
+  --       silent = true,
+  --     },
+  --     lsp = {
+  --       progress = {
+  --         enabled = false,
+  --       },
+  --       -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+  --       override = {
+  --         ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+  --         ['vim.lsp.util.stylize_markdown'] = true,
+  --         ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+  --       },
+  --     },
+  --   }
+  -- end,
+}
