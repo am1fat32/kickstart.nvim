d4bb01c0d7c146ecc1437cfee450773cbaf24870
fat32 <fat32dev@gmail.com>
2024-06-24 18:39:41 +0400

    Update nvim-ts-autotag setup

--- lua/plugins/nvim-ts-autotag.lua
+++ lua/plugins/nvim-ts-autotag.lua
@@ -2,11 +2,13 @@ return {
   'windwp/nvim-ts-autotag',
   config = function()
     require('nvim-ts-autotag').setup {
-      enable = true,
-      enable_rename = true,
-      enable_close = true,
-      enable_close_on_slash = true,
-      filetypes = { 'html', 'xml', 'typescriptreact', 'javascriptreact' },
+      opts = {
+        enable = true,
+        enable_rename = true,
+        enable_close = true,
+        enable_close_on_slash = true,
+        filetypes = { 'html', 'xml', 'typescriptreact', 'javascriptreact' },
+      }
     }
   end,
 }
